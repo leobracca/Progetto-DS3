5ab2132231d110f8c9bf49cf5e59a09b77d6472b
Leobracca <braccalenti.leonardo@franchettisalviani.net>
2026-03-16 08:30:07 +0100

    Creazione classi principali
    -Creazione classe Gestore
    -Creazione classe Inventario
    -Creazione classe Personaggio
    -Creazione classe FileManager

--- Progetto-DS3/src/progetto/ds3/ProgettoDS3.java
+++ Progetto-DS3/src/progetto/ds3/ProgettoDS3.java
@@ -14,7 +14,7 @@ public class ProgettoDS3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prova");
+        System.out.println("Buona sera");
     }
     
 }
