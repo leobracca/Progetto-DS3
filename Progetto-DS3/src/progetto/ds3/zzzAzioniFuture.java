1f51ca9e8267777f4804f321dd8698ba38b3529b
leobracca <braccalenti.leonardo@franchettisalviani.net>
2026-03-17 13:30:20 +0100

    Inizio progettazione eventi
    -Creato documento di testo con tutti i dettagli delle classi per gli eventi
    -Aggiunto all'uml classi e metodi per gestire gli eventi
    -Inizio scrittura codice nel documento creato per poter organizzare le classi e metodi appena create

--- Progetto-DS3/src/progetto/ds3/zzzAzioniFuture.java
+++ Progetto-DS3/src/progetto/ds3/zzzAzioniFuture.java
@@ -1,3 +1,15 @@
 /**
  * Aggiungere gli eventi
+ *      Creazione file contenente tutti i possibili oggetti che si possono trovare
+ *      Leggere il file e metterlo dentro ad una struttura adatta
+ *      Ogni round aggiungere un oggetto in maniera casuale al giocatore
+ * 
+ * Utilizzare gli oggetti
+ *      Ogni round far scegliere se il giocatore vuole usare o no l'oggetto trovato
+ *      L'oggetto usato modifica lo status del giocatore
+ * 
+ * Aggiungere come evento i boss 
+ *      File contenente tutti i boss
+ *      Leggere il file e metterlo dentro ad una struttura dati adatta
+ *      Casualmente generare un boss oppure un oggetto
  */
